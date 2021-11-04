30215d950049e3c67ac0860147ea16abf93b8c54
veasnawt <veasnawt@gmail.com>
2021-11-04 16:53:04 +0700

    Initial Release

--- lib/wrap_if.dart
+++ lib/wrap_if.dart
@@ -1,7 +1,24 @@
 library wrap_if;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class WrapIf extends StatelessWidget {
+  // if conditon is true, wrap with parent; else only child
+  final bool condition;
+  // a function that returns the parent with the child - child
+  final Widget Function(Widget child) parentBuilder;
+  // child of the parent
+  final Widget child;
+
+  const WrapIf({
+    Key? key,
+    required this.condition,
+    required this.parentBuilder,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return condition ? parentBuilder(child) : child;
+  }
 }
